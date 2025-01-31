6ab736a3eaa3650a646445aa5d62d484a46669a3
ayush-trivedi-925 <asxu525@gmail.com>
2025-01-31 12:22:34 +0530

    Added search functionality in side nv - to search pokemon by pokenum and pokename

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,22 +1,40 @@
+import { useState } from "react";
 import { first151Pokemon, getFullPokedexNumber } from "../utils";
 export default function SideNav({ selectedPokemon, setSelectedPokemon }) {
+  const [searchValue, setSearchValue] = useState("");
+  const filteredPokemon = first151Pokemon.filter((ele, eleIdx) => {
+    // if eleIdx includes the current search value, return true
+    if (ele.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())) {
+      return true;
+    }
+    // if ele name includes the current search vlaue, return true
+    if (getFullPokedexNumber(eleIdx).includes(searchValue)) {
+      return true;
+    }
+    // otherwise, exclude value from arry, return false
+    return false;
+  });
   return (
     <nav>
       <div>
         <h1 className="text-gradient">PokeDatabase</h1>
       </div>
-      <input />
-      {first151Pokemon.map((pokemon, pokeIndex) => {
+      <input
+        placeholder="ex: 001 or Balba..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      {filteredPokemon.map((pokemon, pokeIndex) => {
         return (
           <button
-            onClick={() => setSelectedPokemon(pokeIndex)}
+            onClick={() => setSelectedPokemon(first151Pokemon.indexOf(pokemon))}
             key={pokeIndex}
             className={
               "nav-card" +
               (pokeIndex === selectedPokemon ? "nav-card-selected" : "")
             }
           >
-            <p>{getFullPokedexNumber(pokeIndex)}</p>
+            <p>{getFullPokedexNumber(first151Pokemon.indexOf(pokemon))}</p>
             <p>{pokemon}</p>
           </button>
         );
